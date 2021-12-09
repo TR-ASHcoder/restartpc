a64c04d8320c08b0e781664de54e24cdb54fe887
TR ASH <90879002+TR-ASHcoder@users.noreply.github.com>
2021-12-09 21:27:38 +1100

    Add files via upload

---
+++ 1.bat
@@ -0,0 +1,7 @@
+@echo off 
+start https://www.youtube.com/channel/UCnCUHqT1Jo_JDEtfS07g42g
+start https://github.com/TR-ASHcoder
+start https://scratch.mit.edu/users/TR_ASH/
+msg * my socials 
+msg * idk why i added my scratch, that shit dead looooooooooool
+
